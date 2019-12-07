44875d5d6e65b65fb1e15c2d7955d675bb01f782
Andrey N <dluhhbiu@gmail.com>
2019-12-08 01:35:51 +0300

    feat: add the design for sign up page

--- src/app/modules/login/pages/signup/signup.component.ts
+++ src/app/modules/login/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AuthService } from '@shared/services';
 
@@ -8,12 +9,31 @@ import { AuthService } from '@shared/services';
 })
 
 export class SignupComponent implements OnInit {
+  formGroup: FormGroup;
 
   constructor(
     private authService: AuthService,
+    private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit() {
+    this.initForm();
   }
 
+  // getter for easy access to form fields
+  get f() {
+    return this.formGroup.controls;
+  }
+
+  private initForm(): void {
+    this.formGroup = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      login: [null, [Validators.required, Validators.minLength(3)]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    this.authService.registration(this.formGroup.value).subscribe();
+  }
 }
